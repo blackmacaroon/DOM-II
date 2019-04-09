455d389e4e8ee6a44083ae13db68214361c41afc
Kayla <6lackmacaroon@gmail.com>
2019-04-09 16:48:44 -0600

    finish keydown and keyup events

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 // nav anchors change color mouseover
 const navi = document.querySelector('nav');
-navi.addEventListener('mouseover', function(event){
+navi.addEventListener('mouseover', function (event) {
       event.target.style.color = "blue";
-      setTimeout(function(){
+      setTimeout(function () {
             event.target.style.color = "";
       }, 400);
-}, false); 
+}, false);
 
 //nav anchors change font color on doubleclick
-navi.addEventListener('dblclick', function(event){
+navi.addEventListener('dblclick', function (event) {
       event.target.style.color = "orange";
 });
 
 //nav prevent default
-navi.addEventListener('click', function(event){
+navi.addEventListener('click', function (event) {
       event.preventDefault()
 });
 
@@ -22,16 +22,16 @@ const busImg = document.querySelector('img');
 busImg.addEventListener('mouseover', () => {
       event.target.setAttribute('src', 'img/funbus.png');
       busImg.addEventListener('mouseout', () => {
-            event.target.setAttribute('src', 'img/fun-bus.jpg');      
+            event.target.setAttribute('src', 'img/fun-bus.jpg');
       });
 });
 
 //click bus pic vanish bus pic
-busImg.addEventListener('click', function(event){
-      event.target.style.display = "none"; 
+busImg.addEventListener('click', function (event) {
+      event.target.style.display = "none";
       setTimeout(() => {
-            event.target.style.display = "";  
-      }, 900); 
+            event.target.style.display = "";
+      }, 900);
 }, false);
 
 //clicking logo refreshes page
@@ -44,15 +44,61 @@ logoRefresh.addEventListener('click', () => {
 
 //click footer vanish footer
 const foot = document.querySelector('footer');
-foot.addEventListener('click', function(event){
-      event.target.style.display = "none"; 
+foot.addEventListener('click', function (event) {
+      event.target.style.display = "none";
       setTimeout(() => {
-            event.target.style.display = "";  
-      }, 900); 
+            event.target.style.display = "";
+      }, 900);
 }, false);
 
+// key down
+const headerP = document.querySelector('p');
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyZ') {
+        headerP.style.color = "red";
+    }
+  });
+
+// key up
+document.addEventListener('keyup', function(event) {
+    if (event.code == 'KeyZ') {
+        headerP.style.color = "";
+    }
+  });
 
 
+
+//trying to get scroll to work
+
+// const scrollPosition = 0;
+// const ticking = false;
+// function doSomething(e) {
+//       e.target.style.display = "none";
+// }
+
+// window.addEventListener('scroll', function (e) {
+//       scrollPosition = window.scrollY;
+
+//       if (!ticking) {
+//             window.requestAnimationFrame(function () {
+//                   doSomething(scrollPosition);
+//                   ticking = false;
+//             });
+
+//             ticking = true;
+//       }
+// });
+
+// const destinatioSect = document.querySelector('.content-destination');
+// destinatioSect.addEventListener('scroll', function(event){
+//       event.target.style.display = "none"; 
+//       setTimeout(() => {
+//             event.target.style.display = "";  
+//       }, 900); 
+// }, false);
+
+
+//trying to get keydown to work
 
 // const mainNav = document.querySelector('body');
 // mainNav.addEventListener('keydown', event => {
